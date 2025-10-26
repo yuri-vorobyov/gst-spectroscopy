9aa856f4271ffdff0545113a129aa3a027e130ee
Yuri Vorobyov <juriy.vorobjov@gmail.com>
2025-10-26 20:07:26 +0300

    Base class for R&T and n&k spectra.

---
+++ source/SpectrumPair.py
@@ -0,0 +1,157 @@
+import numpy as np
+from pathlib import Path
+import os.path
+from scipy.interpolate import interp1d
+from sg_smooth.smoothing import smSG_bisquare
+import matplotlib.pyplot as plt
+
+
+class SpectrumPair:
+    """
+    Container for a pair of spectra. Horizontal scale is wavelength, and it is stored in nanometers.
+    Values of physical quantities are considered non-negative.
+    Below individual spectra are called first and second.
+    """
+
+    # Each spectrum has its color for built-in plots.
+    COLORS = {
+        'first': '#1f77b5',  # blue-ish
+        'second': '#fd8114'  # red-ish
+    }
+
+    def __init__(self, w, first, second, first_label, second_label):
+        """
+        Parameters
+        ----------
+        w : array-like
+            Array of wavelengths in nanometers.
+        first : array-like
+            First spectrum.
+        second : array-like
+            Second spectrum.
+        first_label : str
+            Label of the first spectrum 
+        second_label : str
+            Label of the second spectrum 
+        """
+        # Check correctness of input arrays.
+        if not (len(w) == len(first) == len(second)):
+            raise Exception('Input arrays lengths must be equal.')
+
+        # Save labels.
+        self.first_label = first_label
+        self.second_label = second_label
+
+        # Ensure spectra are sorted by the wavelength.
+        ii = np.argsort(w)
+        self.w = w[ii]
+        self.first = first[ii]
+        self.second = second[ii]
+
+        # Remove negative values.
+        ii = (self.first > 0) * (self.second > 0)
+        self.w = self.w[ii]
+        self.first = self.first[ii]
+        self.second = self.second[ii]
+
+        # Placeholders for the smoothed version.
+        self.sW = None
+        self.sFirst = None
+        self.sSecond = None
+
+    @property
+    def e(self):
+        """Return photon energy scale in eV for this spectrum."""
+        return 1239.842 / self.w
+
+    @property
+    def sE(self):
+        return 1239.842 / self.sw
+
+    def strip(self, wl_min, wl_max):
+        """
+        Strip the wavelength scale.
+
+        Parameters
+        ----------
+        wl_min : float
+            Minimum wavelength in nm.
+        wl_max : float
+            Maximum wavelength in nm.
+        """
+        ii = (self.w > wl_min) * (self.w < wl_max)
+        self.w = self.w[ii]
+        self.first = self.first[ii]
+        self.second = self.second[ii]
+
+    def resample(self, step=None):
+        """
+        Resample the spectrum using uniform step.
+        """
+        # The `step` is allowed to be inferred from the data. It is the maximum separation between
+        # data points.
+        if step is None:
+            step = (self.w[1:] - self.w[:-1]).max()
+
+        scale = np.linspace(self.w[0], self.w[-1], int(np.ceil((self.w[-1] - self.w[0]) / step)) + 1)
+        self.first = interp1d(self.w, self.first, kind='linear')(scale)
+        self.second = interp1d(self.w, self.second, kind='linear')(scale)
+        self.w = scale
+
+    def calc_smoothed(self, w, n):
+        """
+        Calculate (update) filtered spectra.
+
+        Parameters
+        ----------
+        w : int
+            Radius of smoothing window (number of points).
+        n : int
+            Order of approximating polynomial.
+        """
+        self.sW, self.sFirst, _ = smSG_bisquare(self.w, self.first, w, n, extend=False)
+        _, self.sSecond, _ = smSG_bisquare(self.w, self.second, w, n, extend=False)
+
+    def plot(self, smoothed=False, scale='wavelength', title=''):
+        """
+        Plot the spectrum.
+
+        Parameters
+        ----------
+        smoothed : bool
+            Set this to True to plot smoothed data. Raises an exception in case there is no smoothed data.
+        scale : str
+            Either `wavelength` or `energy`.
+        title : str
+            Optional title.
+        """
+        if scale not in {'wavelength', 'energy'}:
+            raise Exception('`scale` support only "wavelength" or "energy"')
+
+        # Here we assume that "style.mplstyle" is placed near this script.
+        plt.style.use(Path(__file__).parent() / 'style.mplstyle')
+        plt.rcParams['savefig.directory'] = Path(__file__).parent()
+        fig, ax_first = plt.subplots(1, 1)
+        fig.canvas.manager.set_window_title(title)
+        ax_second = ax_first.twinx()
+        ax_first.set_title(title)
+        ax_first.set_xlabel({'wavelength': 'Wavelength (nm)',
+                             'energy': 'Photon energy (eV)'}[scale])
+        ax_first.set_ylabel(self.first_label)
+        ax_second.set_ylabel(self.second_label)
+
+        if smoothed:
+            x = {'wavelength': self.sW, 'energy': self.sE}[scale]
+            y1 = self.sFirst
+            y2 = self.sSecond
+        else:
+            x = {'wavelength': self.w, 'energy': self.e}[scale]
+            y1 = self.first
+            y2 = self.second
+
+        l_first, = ax_first.plot(x, y1, c=SpectrumPair.COLORS['first'], alpha=0.7, label=self.first_label)
+        l_second, = ax_second.plot(x, y2, c=SpectrumPair.COLORS['second'], alpha=0.7, label=self.second_label)
+
+        ax_second.legend(handles=(l_first, l_second), loc='best')  # because "second" is above the "first"
+
+        plt.show(block=True)
